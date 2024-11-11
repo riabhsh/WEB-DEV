0c01979d65139b1d6b6d76d3bdeb2d745c7c654d
Rishabh Maurya <riabhsh@gmail.com>
2024-11-12 00:19:25 +0530

    completed toDesktop Tailwind Clone

--- Tailwind CSS/Anuj Bhaiya/Learn Tailwind CSS/src/script.js
+++ Tailwind CSS/Anuj Bhaiya/Learn Tailwind CSS/src/script.js
@@ -45,9 +45,22 @@ setupIntersectionObserver(line1, true, 0.15);
 setupIntersectionObserver(line2, false, 0.15);
 setupIntersectionObserver(line3, true, 0.15);
 
-const littleline = document.getElementById('scrollLine');
-setupIntersectionObserver(littleline, false, 0.30);
+// const littleline = document.getElementById('scrollLine');
+// setupIntersectionObserver(littleline, false, 0.30);
  
 const line4 = document.getElementById('line4');
 
 setupIntersectionObserver(line4, true, 0.8);
+
+const dtElements = document.querySelectorAll('dt');
+dtElements.forEach(element =>{
+    element.addEventListener('click', () => {
+        const ddId = element.getAttribute('aria-controls');
+        const ddElement = document.getElementById(ddId);
+        const ddArrowIcon = element.querySelectorAll('i')[0];
+
+        ddElement.classList.toggle('hidden');
+        ddArrowIcon.classList.toggle('-rotate-180');
+
+    })
+})
